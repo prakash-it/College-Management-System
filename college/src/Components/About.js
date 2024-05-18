e54de2da5374a63cfb13be4cac4027a0530c1596
PRAKASH <prakashkandasamy4@gmail.com>
2024-05-18 21:13:41 +0530

    about

--- college/src/Components/About.js
+++ college/src/Components/About.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function About() {
+  const [showVisionMission, setShowVisionMission] = useState(false);
+
+  const toggleVisionMission = () => {
+    setShowVisionMission(!showVisionMission);
+  };
+
   return (
-    <div>
-      About
-      
+    <div className="about-section">
+      <div className="row">
+        <div className="col-1">
+          <h4>About PC EDU</h4>
+          <h4>About The College</h4>
+          <h4 onClick={toggleVisionMission}>Vision & Mission</h4>
+        </div>
+        <div className="col-2">
+          {showVisionMission && (
+            <div className="vision-mission">
+              <h5>Vision:</h5>
+              <p>Add your college's vision here</p>
+              <h5>Mission:</h5>
+              <p>Add your college's mission here</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
