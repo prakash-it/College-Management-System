0bfd4d4d68eb634c5ece528501d596939bc0f008
KGM <KGM@CASL6-57-185.kgisledu.com>
2024-05-18 16:08:40 +0530

    nav-finish

--- college/src/App.js
+++ college/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
-
+import Navbar from './Components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../src/Components/Home'
+import About from '../src/Components/About'
+import Contact from '../src/Components/Contact'
 function App() {
   return (
     <div className="App">
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
 
     </div>
   );
--- college/src/Components/Navbar.js
+++ college/src/Components/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'; 
+import './css/Nav.css';
+import  {NavLink}  from 'react-router-dom';
+import logo from '../Img/BlackLogo1.png'; 
 
 export default function Navbar() {
   return (
     <div>
-      <nav>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/about'>About</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
+      <nav className='nav'>
+
+      <img src={logo}  alt="Logo" className="logo" />
+
+        <div className="nav-links">
+          <NavLink to='/' className="nav-item">Home</NavLink>
+          <NavLink to='/about' className="nav-item">About</NavLink>
+          <NavLink to='/course' className="nav-item">Course</NavLink>
+          <NavLink to='/contact' className="nav-item">Contact</NavLink>
+        </div>
+        <button className='btn'><NavLink> CheckInfo</NavLink></button>
       </nav>
     </div>
   );
