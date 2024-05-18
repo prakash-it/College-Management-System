b086d4d4630a1f99e280728d0bcdebbab0003184
PRAKASH <prakashkandasamy4@gmail.com>
2024-05-18 08:31:53 +0530

    nav-work

--- college/src/App.js
+++ college/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Studentdata from './Components/Student-data';
-import Teacherdata from './Components/Teacher-data';
+import Navbar from './Components/Navbar';
+import { Route, Router } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Studentdata/>
-      <Teacherdata/>
+
+      <Navbar />
+      <Router>
+        <Route path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Router>
     </div>
   );
 }
