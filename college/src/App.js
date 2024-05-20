982dd33596e809aafd81d0cc1318116cad4c0fba
PRAKASH <prakashkandasamy4@gmail.com>
2024-05-21 00:25:06 +0530

    Info-page-style

--- college/src/App.js
+++ college/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from '../src/Components/Home'
 import About from '../src/Components/About'
 import Contact from '../src/Components/Contact'
 import Footer from './Components/Footer';
 import Info from './Components/Info';
 import Course from './Components/Course';
+
 function App() {
   return (
     <div className="App">
@@ -17,7 +18,8 @@ function App() {
       <Route path='/about' element={<About/>}/>
       <Route path='/course' element={<Course/>}/>
       <Route path='/contact' element={<Contact/>}/>
-      <Route path='info' element={<Info/>}/>
+      <Route path='/info' element={<Info/>}/>
+     
     </Routes>
 
     <Footer/>
--- college/src/Components/Info.js
+++ college/src/Components/Info.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import './css/Info.css'
-
+import admin from './Img/Admin.jpg'
+import student from './Img/Student.jpg'
+import Teacher from './Img/Teacher.jpg'
 export default function Info() {
+
   return (
     <>
-      <h1>Check your info </h1>
+      <h1 className='check'>Check your info </h1>
+     
+      <div class='containers'>
+  <div class='card__container'>
+    <div class='card__article'>
       
+      <img src={admin} alt='admin'/>
+      <div class='card__data'>
+       
+        <h2 class='card__title'>Admin Login</h2>
+        <a href='#admin' class='card__button'>Click</a>
+      </div>
+    </div>
+
+    <div class='card__article'>
+     
+      <img src={student} alt='student'/>
+      <div class='card__data'>
+       
+        <h2 class='card__title'>Student Login</h2>
+        <a href='#student' class='card__button'>Click</a>
+      </div>
+    </div>
+
+    <div class='card__article'>
+
+      <img src={Teacher} alt='Teacher'/>
+      <div class='card__data'>
+        
+        <h2 class='card__title'>Teacher Login</h2>
+        <a href='#teacher' class='card__button'>Click</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
     </>
   )
 }
