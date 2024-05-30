caa069eca89ee9e82e7eb5afc4abca67ff3bd61f
prakash-it <prakashkandasamy4@gmail.com>
2024-05-31 00:09:23 +0530

    Front-end-work

--- college/src/Components/Studentlogin.js
+++ college/src/Components/Studentlogin.js
@@ -3,6 +3,11 @@ import React from 'react'
 export default function Studentlogin() {
   return (
     <>
+     <form>
+        <input type='email'/>
+        <input type='password'/>
+        <button type='submit'>Submit</button>        
+    </form>
     </>
   )
 }
